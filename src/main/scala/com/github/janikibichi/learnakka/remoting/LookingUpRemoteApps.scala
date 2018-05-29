44c18dd57d7104ad55c0b9360885f26969f96fbc
janikibichi <janikibichi@gmail.com>
2018-05-29 07:47:26 +0300

    update readme to show workflow to actors

--- src/main/scala/com/github/janikibichi/learnakka/remoting/LookingUpRemoteApps.scala
+++ src/main/scala/com/github/janikibichi/learnakka/remoting/LookingUpRemoteApps.scala
@@ -3,17 +3,19 @@ package com.github.janikibichi.learnakka.remoting
 import akka.actor.{ActorRef, ActorSystem, Props}
 import scala.concurrent.duration._
 
+//Select the actor '/remoteActor' and send it a message
 object LookUpActorSelection extends App{
   val actorSystem = ActorSystem("LookingUpActors")
 
   implicit val dispatcher = actorSystem.dispatcher
 
-  //Create and define Actor in ActorSystem
+  //Look for the Actor using Actor Selection - This returns a future
   val selection = actorSystem.actorSelection("akka.tcp://LookingUpRemoteActors@127.0.0.1:2553/user/remoteActor")
 
-  //Send Message to Actors
+  //Send Message to Future
   selection ! "test"
 
+  //If resolved after 3 seconds, send a Message :: OnSuccess instead of Await.result which is blocking
   selection.resolveOne(3 seconds).onSuccess {
     case actorRef: ActorRef =>
       println("We got an ActorRef")
@@ -21,6 +23,8 @@ object LookUpActorSelection extends App{
   }
 }
 
+
+//Create the Actor and name it 'remoteActor'
 object LookingUpRemoteActors extends App {
   val actorSystem = ActorSystem("LookingUpRemoteActors")
   actorSystem.actorOf(Props[SimpleActor],"remoteActor")
